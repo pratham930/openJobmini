0708275de6e04d7d3ad64092c5e8b94fb044c45b
pratham930 <priyatam.patel17@gmail.com>
2022-10-16 08:28:30 -0700

    minor changes
    minor changes

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
+dotenv.config({path:'./config.env'})
 
 import connectDB from "./db/connectdb.js";
 
 // import Razorpay from "razorpay";
-import authRoute from "./routes/auth.js";
-import userRoute from "./routes/user.js";
+import authRoute from "./routes/employee.js";
+import userRoute from "./routes/admin.js";
 
 const port = process.env.PORT || '9000'
 const DATABASE_URL = process.env.DATABASE_URL|| "mongodb://localhost:27017"
@@ -19,7 +20,6 @@ connectDB(DATABASE_URL)
 export  const app = express()
  app.use(cookieParser())
 app.use(express.json())
-dotenv.config({path:'./config.env'})
 app.use(cors())
 app.use(express.static('public/uploads/pimage'))
 app.use(express.static('public/uploads/rdoc'))
@@ -28,26 +28,6 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use('/api', authRoute);
 app.use('/api', userRoute);
-// app.use('/api', categoryRoute);
-// app.use('/api', paymentRoute);
-// app.use('/api', cartRoute);
-// app.use('/api', detailsRoute);
-// app.use('/api', soldRoute);
-// app.use('/sms.activesms.in/app/smsapi/index.php?', smsdRoute);
-// app.use('sms.activesms.in/app/smsapi/index.php?key=5632EB557E6513&campaign=0&routeid=26&type=text&contacts=9302009469&senderid=SFTWRE&msg=Hello+People%2C+have+a+great+day&template_id= "', smsdRoute)
-
-
-// app.get('/contact',(req,res) =>{
-//      res.cookie('test',"thapa")
-//     res.send("set hoja cookies")
-// })
-
-
-
-
-//   app.get("/api/getkey", (req, res) =>
-//   res.status(200).json({ key: process.env.RAZORPAY_API_KEY })
-// );
 
 
 app.listen(port, ()=>{
